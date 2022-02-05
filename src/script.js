8abfe57e89b402288bc1443a610c9bf5a4d0f646
cedcoss@nkit <ankitaginhotry@cedcoss.com>
2022-02-05 10:34:57 +0530

    Understanding Uninitialized Variables

--- src/script.js
+++ src/script.js
@@ -8,3 +8,14 @@ yourname=myname;
 var a=9;
 
 var youname="Sachin Shukla";
+var myFirstName="Ankit";
+var myLastName="Aginhotry";
+
+var a=5;
+var b=10;
+var c="I am a";
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
