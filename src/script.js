13d045b1a5a36470d71729cbb638b85644618b28
cedcoss@nkit <ankitaginhotry@cedcoss.com>
2022-02-05 12:18:12 +0530

    Array fached in Multi-Dimensional

--- src/script.js
+++ src/script.js
@@ -188,3 +188,13 @@ console.log(nestedarr[1]);
 //Modify Array Data With Indexes
 nestedarr[0]="zero";
 console.log(nestedarr[0]);
+//Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+  console.log(myData);
